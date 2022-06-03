d5b8a7cf74b7d8aedd46fc075be93725846c1f33
maaarj <65552059+maaarj@users.noreply.github.com>
2022-06-04 01:19:47 +0800

    card, @apply, btn
    btn is work on progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    /*fontSize: {
-      '2mob': '2.5rem'
-    }*/
-    extend: {},
+    /**/
+    extend: {
+      fontSize: {
+        //mobile
+        'mdisplay1': '2.5rem', // 40px
+        'mh1': '2rem', // 32px
+        'mh3': '1.375rem', // 22px
+        'mdparagrap': '1.188rem', // 19px
+        
+        //tablet + desktop
+        'dh1': '2.5rem', // 40px
+        'ddisplay2': '2rem', // 32px
+
+      }
+    },
   },
   plugins: [],
 }
